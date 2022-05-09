8ee7571113b6f929b1cae713d162d85a27c308f2
Mancuso Antonio <espedito.mancuso@istitutotagnelli.it>
2022-05-09 12:22:38 +0200

    version 1.0

--- PCTOgen.py
+++ PCTOgen.py
@@ -12,11 +12,15 @@ DOCX_TEMPLATES = [
     '03-Patto_formativo_studente.docx',
     '04-Progetto_formativo_studente.docx',
     '05-Foglio_presenze.docx',
-    '06-Scheda_di_valutazione_studente.docx'
+    '06-Scheda_di_valutazione_studente.docx',
+    '07-Modulo_di_gradimento_studente.docx',
+    '08-Relazione_di_fine_attività.docx'
 ]
 
-def render_pdf(in_path, out_path):
+def student_to_path(student):
+    return student.replace(" ", "_")
 
+def render_pdf(in_path, out_path):
     subprocess.call(['soffice',
                  '--headless',
                  '--convert-to',
@@ -24,7 +28,11 @@ def render_pdf(in_path, out_path):
                  '--outdir',
                  out_path,
                  in_path])
-    return in_path
+
+
+def render_PCTO_pdf(studente):
+    log.info("Generating final PCTO document file for student {}".format(studente)) 
+    os.system("pdftk " + studente + "/*.pdf cat output " + studente + "/" + studente + "_PCTO_TO_PRINT.PDF")
 
 def render_docx(studente, fpath, context):
     if not os.path.exists(fpath):
@@ -32,9 +40,13 @@ def render_docx(studente, fpath, context):
         quit()  
     doc = DocxTemplate(fpath)
     doc.render(context)
-    doc.save(studente.replace(" ", "_") + "_" + fpath)
+    doc_dir = student_to_path(studente)
+    if not os.path.exists(doc_dir):
+        os.mkdir(doc_dir)
+    doc_path = doc_dir + "/" + student_to_path(studente) + "_" + fpath
+    doc.save(doc_path)
     log.info("Generating {} file for student {}".format(fpath, studente))  
-    render_pdf(studente.replace(" ", "_") + "_" + fpath, studente.replace(" ", "_"))
+    render_pdf(doc_path, studente.replace(" ", "_"))
     log.info("Rendering PDF version of {} document for student {}".format(fpath, studente))  
 
 def read_validate_PCTODB(fpath):
@@ -65,7 +77,8 @@ def get_ws_cells(ws, col, rows, rowe):
 
     cells = {}
     for row in range(rows, rowe + 1):
-        cells[ws.cell(column=col, row=row).value] = ws.cell(column=col+1, row=row).value
+        if ws.cell(column=col+1, row=row).value is not None:
+            cells[ws.cell(column=col, row=row).value] = ws.cell(column=col+1, row=row).value
     return cells
 
 def main(args):
@@ -84,21 +97,24 @@ def main(args):
     pcto = get_ws_cells(wb['PCTO'], col = 1, rows = 3, rowe = 21)
     azienda = get_ws_cells(wb['AZIENDA'], col = 1, rows = 3, rowe = 12)
     studenti_misc = get_ws_cells(wb['STUDENTI'], col = 1, rows = 3, rowe = 4)
-    studenti_names = get_ws_cells(wb['STUDENTI'], col = 1, rows = 8, rowe = 10)
+    studenti_names = get_ws_cells(wb['STUDENTI'], col = 1, rows = 8, rowe = 27)
     context = pcto | azienda | studenti_misc
+    #Generates documents package for each student
     for student in studenti_names:
+        studente = studenti_names[student] 
         log.info("")
-        log.info("Generating PCTO documentation for {}".format(studenti_names[student]))
-        context['STUDENTE'] = studenti_names[student] 
+        log.info("Generating PCTO documentation for {}".format(studente))
+        context['STUDENTE'] = studente 
         for doc in DOCX_TEMPLATES:
-            render_docx(studenti_names[student], doc, context)
+            render_docx(studente, doc, context)
+        render_PCTO_pdf(student_to_path(studente))
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
                       description='Automatically generate PCTO documentation \
-                                   package starting from an excel file')
+                                   package starting from an excel file. Libreoffice and pdftk are needed')
     parser.add_argument('-i',
                         help='Input PCTO DB file in .xlsx format', required=True)
    
